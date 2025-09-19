1eb4dc19d0fb855da0ec3b08c7e1b35c1216fc30
haifaghribi <152281844+haifaghribi@users.noreply.github.com>
2025-09-19 13:59:00 +0100

    Update portfolio with new edits

--- components.js
+++ components.js
@@ -85,7 +85,7 @@ const HeroSection = ({ scrollToSection, darkMode }) => {
                     <div className="md:w-1/2 flex justify-center">
                         <div className="relative w-64 h-64 md:w-80 md:h-80 rounded-full overflow-hidden border-4 border-blue-500 shadow-xl">
                             <img 
-                                src="/profile.png" 
+                                src="./public/profile.png"
                                 alt="Profile" 
                                  className="w-full h-full object-cover"
                             />
